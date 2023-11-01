27851a5e7920ab1953024d390091544f6adce9ef
Paulo Chaves <pauloariell@live.com>
2023-10-31 21:15:59 -0300

    Estrutura visual da Home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,28 @@
-import { Heading } from '@ignite-ui/react'
+import { Heading, Text } from '@ignite-ui/react'
+import Image from 'next/image'
+import { Container, Hero, Preview } from './styles'
+
+import previewimage from '../../assets/app-preview.png'
 
 export default function Home() {
-  return <Heading as="h1">Hello world</Heading>
+  return (
+    <Container>
+      <Hero>
+        <Heading size="4xl">Agendamento descomplicado</Heading>
+        <Text size="xl">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+      </Hero>
+      <Preview>
+        <Image
+          src={previewimage}
+          height={400}
+          quality={100}
+          priority
+          alt="Calendario simbolizando aplicação em funcionamento"
+        />
+      </Preview>
+    </Container>
+  )
 }
